7f6e568da0df4cb1b81af9998dca4a91c44b7875
Song Uylong PC <uylongsong@gmail.com>
2024-10-06 11:57:47 +0700

    I GOT 1OVER4 DONE

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+import authRoutes from './routes/auth.routes.js';
+import connectToMongoDB from './db/connectToMongoDB.js';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+dotenv.config();
+
+app.use(express.json()); // To parse the incoming request with JSON payloads
+
+app.use("/api/auth",authRoutes)
+
+// app.get('/',(req,res)=>{
+//     res.send("Hello World!!!");
+// })
+
+
+app.listen(PORT,()=>{
+    connectToMongoDB();
+    console.log(`Server is running on port ${PORT}`);
+})
