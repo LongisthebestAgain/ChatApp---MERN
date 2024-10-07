6feb7bf9d48a1ecf1ff869dbbb2f736b1766817a
uylongLaptop <uylongsong@gmail.com>
2024-10-07 14:40:11 +0700

    get into front end

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+            <div className='h-full w-full p-6 bg-purple-500 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100 shadow-md'>
+                <h1 className='text-3xl font-semibold text-center text-gray-300'> Login
+
+                <span className='text-blue-700'> ChatApp</span>
+                </h1>
+
+                
+            </div>
+        </div>
+    )
+}
+
+export default Login
