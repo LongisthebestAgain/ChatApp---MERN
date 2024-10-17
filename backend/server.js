eec2b69c16c5eced2fdcdcfc7c6a82dc3879420f
Song Uylong PC <uylongsong@gmail.com>
2024-10-17 14:14:49 +0700

    deployment

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,12 @@ app.use(cookieParser()); // To parse the incoming cookies
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
+
 app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));    
+})
 
 server.listen(PORT,()=>{ // start the server with socket.io
     connectToMongoDB();
